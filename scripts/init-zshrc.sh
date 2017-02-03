7d9d5e82b75137843fe897521b7c467fccbef689
Scott Pringle <pringlesc91@gmail.com>
2017-02-03 21:14:39 +0000

    Added a Symfony installer alias

--- scripts/init-zshrc.sh
+++ scripts/init-zshrc.sh
@@ -5,6 +5,7 @@ command -v zsh >/dev/null 2>&1
 if [ $? -ne 0 ];
 then
 	sudo apt-get install zsh -y
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 cp zshrc/config $HOME/.zshrc
